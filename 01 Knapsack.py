cbbb37426feda8b8e463365ef1c0f16905f40778
lchua2314 <46981223+lchua2314@users.noreply.github.com>
2019-11-11 22:13:46 -0800

    Documented 0/1 Knapsack

--- 01 Knapsack.py
+++ 01 Knapsack.py
@@ -1,5 +1,5 @@
 #Method that creates and returns the list that has the best profits 
-#based on weight of knapsack (0 to knapsackSize) vs. object's weight (object 1, object 1 + object 2, etc)
+#based on weight of knapsack (0 to knapsackSize) vs. object's weight (object 1 to object 1 + object 2, etc)
 def createSizeVsObjList(knapsackSize, weight, price, num): 
     newList = [[0 for _ in range(knapsackSize + 1)] for _ in range(num + 1)] 
   
@@ -8,10 +8,12 @@ def createSizeVsObjList(knapsackSize, weight, price, num):
         for weightOfCol in range(knapsackSize + 1): 
             if weightOfCol == 0 or i == 0: #If object # is 0 or knapsack size is 0, profit will be 0 in that cell. 
                 newList[i][weightOfCol] = 0
-            elif weight[i-1] > weightOfCol: 
+            elif weight[i-1] > weightOfCol: #If the previous object's weight is greater than to the current knapsack size in this loop,
+                                            #Update newList's current cell to the previous object's weight above the current cell.
                 newList[i][weightOfCol] = newList[i-1][weightOfCol]
-            elif weight[i-1] <= weightOfCol: #If the previous weight is less than or equal to the current knapsack size in this loop,
-                #Update newList's current cell to either the current object's price plus 
+            elif weight[i-1] <= weightOfCol: #If the previous object's weight is less than or equal to the current knapsack size in this loop,
+                #Update newList's current cell to either the previous object's price plus the previous object's (row) and the current column minus the last object's weight
+                #OR the previous object's weight above the current cell based on which one has a higher max. 
                 newList[i][weightOfCol] = max(price[i-1] + newList[i-1][weightOfCol-weight[i-1]],  newList[i-1][weightOfCol])  
 
     return newList
@@ -42,35 +44,50 @@ def print1DList(inputList):
 #Prints the results of profit calculation (showing what objects were taken), 
 #total weight (which should equal to knapsack's size),
 #and total profit.
-def findSolSack(twoDlist, priceList, weightList, weightOfKnapsack, numberOfObj):
+def findSolSack(twoDList, priceList, weightList, weightOfKnapsack, numberOfObj):
 
-    row = numberOfObj
+    row = numberOfObj #row and col start at the very last cell that was updated in createSizeVsObjList() method AKA the solution's profit.
     col = weightOfKnapsack
 
-    totalWeight = 0
+    totalWeight = 0 #Calculates the weight and profit everytime an object is added to knapsack (Not literally, but through text).
     totalProfit = 0
 
-    while twoDlist[row][col] != 0 or row < 0 or col < 0:
+    #While the cell that is being checked is not 0 (which means that it is in either/or
+    #the first row or column which is 0 knapsack size or 0 objects to be placed)
+    #or the row or col is a negative index 
+    while twoDList[row][col] != 0 or row < 0 or col < 0:
 
-        while twoDlist[row-1][col] == twoDlist[row][col]:
+        #If the above cell is equal to the current cell, move up. Repeat until they are not.
+        while twoDList[row-1][col] == twoDList[row][col]:
             row -= 1
 
-        col -= weightList[row-1]
-        totalProfit += priceList[row-1]
-        totalWeight += weightList[row-1]
+        #When the above cell is no longer equal to the current cell, 
+        totalProfit += priceList[row-1] #Add the current object's profit to the total profit 
+                                        #(index is subtracted by 1 because n+1 rows. n being number of objects)
+        totalWeight += weightList[row-1] #Add the current object's weight to the total weight (index same with price)
+        #Inform user of what object is being taken, it's price, it's weight, 
+        #current profit and current weight from current and previous objects taken
         print("Taking object", row, "\nPrice:", priceList[row-1] ,"Weight:", weightList[row-1], "\nCurrent Profit:", totalProfit,"Current Weight:", totalWeight ,"\n")
-        row -= 1
+        col -= weightList[row-1] #Move the column by the weight of the object.
+        row -= 1 #Move up a cell even if it does not match the current cell's profit so same item is not taken twice.
     
+    #After the current cell's item is found to be 0 or the row or col indices are negative, 
+    #all objects are taken that leads to the most optimal solution that is the highest profit of objects
+    #and equal or below the knapsack size
+
+    #Print to the user the total weight (should be equal or below knapsack size) 
+    #and total profit based on the object(s) taken.
     print("\nTotal Weight: ", totalWeight, "\nTotal Profit: ", totalProfit)
 
 #Driver
+#Prompt and input user's knapsack size. Has input validation in which knapsack size cannot be less than or equal to 0.
 while 1:
-    knapsackSize = int(input("Knapsack Size: ")) #Prompt and input user's knapsack size.
+    knapsackSize = int(input("Knapsack Size: ")) 
     if knapsackSize <= 0:
         print("Please enter a value that is greater than 0")
     else:
         break
-#Prompt user for number of objects and input int value into numOfObj
+#Prompt user for number of objects and input int value into numOfObj. Has input validation.
 numOfObj = 0
 while numOfObj <= 0:
     numOfObj = eval(input("Number of Objects: "))
@@ -81,7 +98,7 @@ while numOfObj <= 0:
 objectPrices = [None for _ in range(numOfObj)]
 objectWeights = [None for _ in range(numOfObj)]
 
-#Prompt user and input object's price then weight in decsending order.
+#Prompt user and input object's price then weight in decsending order. Has input validation.
 for objNum in range(numOfObj):
     while 1:
         objectPrices[objNum] = int(input("Object " + str(objNum + 1) + " Price: "))
@@ -103,10 +120,15 @@ print("\nList of weights based on object number from descending order")
 print1DList(objectWeights)
 
 print()
+#Create the list with many smaller portions based on knapsack weight and objects that can be taken.
 sizeVsObj = createSizeVsObjList(knapsackSize, objectWeights, objectPrices, numOfObj)
+#Print the list in a 2D table.
 print2DList(sizeVsObj, knapsackSize)
 print()
 
+#Goes through the 2D list based on the method learned in class to find the objects to put in the knapsack
+#and prompts the user of the object(s) that were picked up with the object's weight and price 
+#along with keeping a total weight and total profit that will be displayed at the end.
 findSolSack(sizeVsObj, objectPrices, objectWeights, knapsackSize, numOfObj)
 
 exit = input("\nEnter anything to exit program: ")
